bbe274de512add2ae53a73c30f59cd343824fa92
Евгений Волков <vamper.2012@gmail.com>
2020-01-16 10:58:13 +0800

    first

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,6 @@ gulp.task('styles', function() {
 // JS
 gulp.task('scripts', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
